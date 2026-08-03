b10ccbf193bd174c117134ecbc38ebf7416775fa
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-03 14:05:38 -0700

    Initialize repository

---
+++ README.md
@@ -0,0 +1,9 @@
+# @hellacardgames/lib
+
+A TypeScript library shared by the various card game implementation libraries.
+
+## Installation
+
+```bash
+npm install @hellacardgames/lib
+```
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { changeTurn } from "./lib/changeTurn.js";
+export { emitEvent } from "./lib/emitEvent.js";
+export { emitEventToPlayer } from "./lib/emitEventToPlayer.js";
+export { removePlayer } from "./lib/removePlayer.js";
+export { shuffle } from "./lib/shuffle.js";
