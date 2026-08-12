a69a1469fab30a78b83704541310d47728259e88
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-12 16:44:55 -0700

    Amend previous commit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { changeTurn } from "./lib/changeTurn.js";
 export { emitEvent } from "./lib/emitEvent.js";
 export { emitEventToPlayer } from "./lib/emitEventToPlayer.js";
 export { getCurrentPlayer } from "./lib/getCurrentPlayer.js";
+export { getEventsAndClearAcknowledged } from "./lib/getEventsAndClearAcknowledged.js";
 export { getNextPlayer } from "./lib/getNextPlayer.js";
 export { isCurrentPlayer } from "./lib/isCurrentPlayer.js";
 export { prependItemToCollection } from "./lib/prependItemToCollection.js";
