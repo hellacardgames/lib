7d0eba2ea496c67ba391cc76ca49f364856152dd
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-25 10:31:13 -0700

    Remove currentPlayer from getClientStateAndClearEventsFactory

--- src/lib/getClientStateAndClearEventsFactory.ts
+++ src/lib/getClientStateAndClearEventsFactory.ts
@@ -1,31 +1,22 @@
-import { getCurrentPlayer } from "./getCurrentPlayer.js";
 import { updatePlayer } from "./updatePlayer.js";
 
 type Game = {
   players: readonly {
     id: string;
   }[];
-  currentPlayerIndex: number;
 };
 
 export function getClientStateAndClearEventsFactory<
   TGame extends Game,
   TClientState extends object,
->(
-  transform: (
-    game: TGame,
-    player: TGame["players"][number],
-    currentPlayer: TGame["players"][number],
-  ) => TClientState,
-) {
+>(transform: (game: TGame, player: TGame["players"][number]) => TClientState) {
   return (game: TGame, playerId: string) => {
     const player = game.players.find((p) => p.id === playerId);
     if (!player) {
       return { success: false, error: "playerNotFound" } as const;
     }
 
-    const currentPlayer = getCurrentPlayer(game);
-    const state = transform(game, player, currentPlayer);
+    const state = transform(game, player);
 
     game = updatePlayer(game, player.id, (p) => ({ ...p, events: [] }));
 
